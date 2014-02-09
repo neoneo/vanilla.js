be6ee3acdd25759a121d46b00ae0f35c3828bba2
Jeroen Knoef <jeroen@neoneo.nl>
2014-02-09 17:20:55 +0100

    Removed most css properties, added api for registering them

--- vanilla.js
+++ vanilla.js
@@ -389,55 +389,6 @@
 
 	};
 
-	// CSS PROPERTIES
-	var cssProperties = ("align-content,align-items,align-self,animation,animation-delay,animation-direction,animation-duration,animation-fill-mode," +
-		"animation-iteration-count,animation-name," +
-		"animation-play-state,animation-timing-function,background,background-attachment,background-clip,background-color," +
-		"background-image,background-origin,background-position,background-repeat,background-size,border,border-bottom,border-bottom-color," +
-		"border-bottom-left-radius,border-bottom-right-radius,border-bottom-style,border-bottom-width,border-collapse,border-color," +
-		"border-image,border-image-outset,border-image-repeat,border-image-slice,border-image-source,border-image-width,border-left," +
-		"border-left-color,border-left-style,border-left-width,border-radius,border-right-border-right-color,border-right-style," +
-		"border-right-width,border-spacing,border-style,border-top,border-top-color,border-top-left-radius,border-top-right-radius," +
-		"border-top-style,border-top-width,border-width,bottom,box-shadow,box-sizing,break-after,break-before,caption-side,clip,clip-path," +
-		"color,columns,column-count,column-fill,column-gap,column-rule,column-rule-color,column-rule-style,column-rule-width,column-span," +
-		"column-width,content,counter-increment,counter-reset,cursor,direction,display,empty-cells,filter,flex,flex-basis,flex-direction," +
-		"flex-flow,flex-grow,flex-shrink,flex-wrap,float,font,font-family,font-feature-settings,font-size,font-size-adjust,font-stretch," +
-		"font-style,font-variant,font-weight,height,image-rendering,image-orientation,ime-mode,justify-content,left,letter-spacing," +
-		"line-height,list-style,list-style-image,list-style-position,list-style-type,margin,margin-bottom,margin-left,margin-right," +
-		"margin-top,max-height,max-width,min-height,min-width,opacity,order,orphans,outline,outline-color,outline-offset,outline-style," +
-		"outline-width,overflow,overflow-wrap,overflow-x,overflow-y,padding,padding-bottom,padding-left,padding-right,padding-top," +
-		"page-break-after,page-break-before,page-break-inside,perspective,perspective-origin,pointer-events,position,quotes,resize," +
-		"right,table-layout,tab-size,text-align,text-align-last,text-decoration,text-decoration-color,text-decoration-line," +
-		"text-decoration-style,text-indent,text-overflow,text-rendering,text-shadow,text-transform,text-underline-position,top,transform," +
-		"transform-origin,transform-style,transition,transition-delay,transition-duration,transition-property,transition-timing-function," +
-		"unicode-bidi,unicode-range,vertical-align,visibility,white-space,widows,width,word-break,word-spacing,word-wrap,writing-mode,z-index,zoom"
-		).split(",");
-	var jsProperties = cssProperties.map(function (cssName) {
-		return cssName === "float" ? "cssFloat" :
-			cssName.split("-").map(function (part, index) {
-				return index === 0 ? part : part.charAt(0).toUpperCase() + part.slice(1);
-			}).join("");
-	})
-
-	cssProperties.forEach(function (cssName, index) {
-		var jsName = jsProperties[index];
-		Object.defineProperty(Style.prototype, jsName, {
-			get: function () {
-				return this.getPropertyValue(cssName);
-			},
-			set: function (value) {
-				if (isArrayLike(value)) {
-					var length = value.length;
-					this._collection.forEach(function (element, index) {
-						element.style.setProperty(cssName, value[index % length]);
-					});
-				} else {
-					this.setProperty(cssName, value);
-				}
-			}
-		});
-	});
-
 	// VANILLA SINGLE ELEMENT =====================================================================
 
 	function one() {
@@ -538,9 +489,29 @@
 
 	};
 
-	cssProperties.forEach(function (cssName, index) {
-		// Getter and setter.
-		var jsName = jsProperties[index];
+	// CSS PROPERTIES
+	vanilla.registerCSSProperty = function (cssName) {
+		var jsName = cssName === "float" ? "cssFloat" :
+				cssName.split("-").map(function (part, index) {
+					return index === 0 ? part : part.charAt(0).toUpperCase() + part.slice(1);
+				}).join("");
+
+		Object.defineProperty(Style.prototype, jsName, {
+			get: function () {
+				return this.getPropertyValue(cssName);
+			},
+			set: function (value) {
+				if (isArrayLike(value)) {
+					var length = value.length;
+					this._collection.forEach(function (element, index) {
+						element.style.setProperty(cssName, value[index % length]);
+					});
+				} else {
+					this.setProperty(cssName, value);
+				}
+			}
+		});
+
 		Object.defineProperty(StyleOne.prototype, jsName, {
 			get: function () {
 				return this.getPropertyValue(cssName);
@@ -549,7 +520,14 @@
 				this.setProperty(cssName, value);
 			}
 		});
-	});
+
+	}
+
+	vanilla.registerCSSProperties = function (cssNames) {
+		cssNames.forEach(vanilla.registerCSSProperty);
+	}
+
+	vanilla.registerCSSProperties(("bottom,cursor,display,float,height,left,opacity,position,right,top,visibility,width,z-index").split(","));
 
 	// EXPORT =====================================================================================
 
